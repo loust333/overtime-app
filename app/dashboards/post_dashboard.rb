b8f7ff842b8553357c0304d465841478c6e77320
Steve Lourenco <lourenco.steve@outlook.com>
2017-03-21 21:08:03 +0100

    Added status to admindashboard

--- app/dashboards/post_dashboard.rb
+++ app/dashboards/post_dashboard.rb
@@ -1,4 +1,4 @@
-require "administrate/base_dashboard"
+require 'administrate/base_dashboard'
 
 class PostDashboard < Administrate::BaseDashboard
   # ATTRIBUTE_TYPES
@@ -13,7 +13,8 @@ class PostDashboard < Administrate::BaseDashboard
     date: Field::DateTime.with_options(searchable: false),
     rationale: Field::Text.with_options(searchable: true),
     created_at: Field::DateTime.with_options(searchable: false),
-    updated_at: Field::DateTime.with_options(searchable: false)
+    updated_at: Field::DateTime.with_options(searchable: false),
+    status: Field::Text.with_options(searchable: true)
   }.freeze
 
   # COLLECTION_ATTRIBUTES
@@ -23,7 +24,7 @@ class PostDashboard < Administrate::BaseDashboard
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = [
     :user,
-    :id,
+    :status,
     :date,
     :rationale
   ].freeze
@@ -32,6 +33,7 @@ class PostDashboard < Administrate::BaseDashboard
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = [
     :user,
+    :status,
     :id,
     :date,
     :rationale,
@@ -45,7 +47,8 @@ class PostDashboard < Administrate::BaseDashboard
   FORM_ATTRIBUTES = [
     :user,
     :date,
-    :rationale
+    :rationale,
+    :status
   ].freeze
 
   # Overwrite this method to customize how posts are displayed
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,15 @@
 )
 puts '1 user created'
 
+@admin = AdminUser.create(
+  email: 'admin@test.com',
+  password: 'testtest',
+  password_confirmation: 'testtest',
+  first_name: 'Admin',
+  last_name: 'User'
+)
+puts '1 admin user created'
+
 100.times do |post|
   Post.create(date: Date.today, rationale: "#{post} rationale content",
               user_id: @user.id)
