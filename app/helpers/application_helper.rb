b5a0e5b79a1fe8ead749556e950ecb3e162c7656
Steve Lourenco <lourenco.steve@outlook.com>
2017-03-24 23:15:42 +0100

    Implemented status label in the application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,25 @@ module ApplicationHelper
   def admin_types
     ['AdminUser']
   end
+
+  def status_label(status)
+    status_span_generator(status)
+  end
+
+  private
+
+  def status_span_generator(status)
+    case status
+    when 'submitted'
+      content_tag(:span, status.titleize, class: 'badge badge-primary')
+    when 'approved'
+      content_tag(:span, status.titleize, class: 'badge badge-success')
+    when 'rejected'
+      content_tag(:span, status.titleize, class: 'badge badge-danger')
+    when 'pending'
+      content_tag(:span, status.titleize, class: 'badge badge-primary')
+    when 'confirmed'
+      content_tag(:span, status.titleize, class: 'badge badge-success')
+    end
+  end
 end
--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -1,5 +1,7 @@
 # app/models/audit_log.rb
 class AuditLog < ApplicationRecord
+  enum status: { pending: 0, confirmed: 1 }
+
   belongs_to :user
 
   validates_presence_of :status, :start_date
