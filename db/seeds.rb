06d1cb794917251be27f1e3736da7cb7eff47e15
Steve Lourenco <lourenco.steve@outlook.com>
2017-03-26 22:28:21 +0200

    Styled the employee homepage pending audit approvals

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,14 +16,16 @@ puts '1 admin user created'
 
 100.times do |post|
   Post.create!(date: Date.today, rationale: "#{post} rationale content",
-              overtime_request: 2.5, user_id: @user.id)
+               overtime_request: 2.5, user_id: @user.id)
 end
 
 puts '100 Posts have been created'
 
-100.times do |audit_log|
-  AuditLog.create!(user_id: @user.id, status: 0,
-                  start_date: (Date.today - 6.days))
-end
+AuditLog.create!(user_id: @user.id, status: 0,
+                 start_date: (Date.today - 6.days))
+AuditLog.create!(user_id: @user.id, status: 0,
+                 start_date: (Date.today - 13.days))
+AuditLog.create!(user_id: @user.id, status: 0,
+                 start_date: (Date.today - 20.days))
 
 puts '100 Audit Logs have been created'
