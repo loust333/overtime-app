632a4226ea3090a7453cd1a9e300a03a2270c766
Steve Lourenco <lourenco.steve@outlook.com>
2017-03-26 22:50:27 +0200

    Implemented functional buttons for employee homepage confirmations

--- app/policies/audit_log_policy.rb
+++ app/policies/audit_log_policy.rb
@@ -1,7 +1,12 @@
 # app/policies/audit_log_policy.rb
 class AuditLogPolicy < ApplicationPolicy
   def index?
+    # TODO: Refactor
     return true if admin?
+  end
+
+  def confirm?
+    record.user_id == user.id
   end
 
   private
