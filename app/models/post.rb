dcef13e36e160723f9a6753138dcb8a875adc77d
Steve Lourenco <lourenco.steve@outlook.com>
2017-03-28 00:31:42 +0200

    Implemented nil guard to fix spec

--- app/models/post.rb
+++ app/models/post.rb
@@ -15,6 +15,6 @@ class Post < ApplicationRecord
   def update_audit_log
     audit_log = AuditLog.where(user_id: user_id,
                                start_date: (self.date - 7.days..self.date)).last
-    audit_log.confirmed!
+    audit_log.confirmed! if audit_log
   end
 end
