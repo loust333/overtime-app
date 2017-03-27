dfb774b3e6f3d1011566650889ae72ce5c6b0fa1
Steve Lourenco <lourenco.steve@outlook.com>
2017-03-28 00:48:15 +0200

    Send sms task and sti for employee

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,31 +1,31 @@
 # db/seeds.rb
 
-@user = User.create!(
+@employee = Employee.create!(
   email: 'test@test.com', password: 'testtest',
   password_confirmation: 'testtest', first_name: 'Jon', last_name: 'Hollywood',
   phone: '4321567834'
 )
-puts '1 user created'
+puts '1 employee created'
 
 @admin = AdminUser.create!(
   email: 'admin@test.com', password: 'testtest',
-  password_confirmation: 'testtest', first_name: 'Admin', last_name: 'User',
+  password_confirmation: 'testtest', first_name: 'Admin', last_name: 'employee',
   phone: '4321563478'
 )
 puts '1 admin user created'
 
-AuditLog.create!(user_id: @user.id, status: 0,
+AuditLog.create!(user_id: @employee.id, status: 0,
                  start_date: (Date.today - 6.days))
-AuditLog.create!(user_id: @user.id, status: 0,
+AuditLog.create!(user_id: @employee.id, status: 0,
                  start_date: (Date.today - 13.days))
-AuditLog.create!(user_id: @user.id, status: 0,
+AuditLog.create!(user_id: @employee.id, status: 0,
                  start_date: (Date.today - 20.days))
 
 puts '3 Audit Logs have been created'
 
 100.times do |post|
   Post.create!(date: Date.today, rationale: "#{post} rationale content",
-               overtime_request: 2.5, user_id: @user.id)
+               overtime_request: 2.5, user_id: @employee.id)
 end
 
 puts '100 Posts have been created'
--- lib/tasks/notification.rake
+++ lib/tasks/notification.rake
@@ -3,20 +3,18 @@ namespace :notification do
         overtime or not'
   task sms: :environment do
     if Time.now.sunday?
-      # 1. Schedule to run at Sunday at 5pm
-      # 2. Iterate over all employees
-      # 3. Skip AdminUsers
-      # 4. Send a message that has instructions and a link to log time
+      employees = Employee.all
+      notification_message = "Please log into the overtime management dashboard
+        to request overtime or confirm your hours for last week: http://test.com"
+      employees.each do |employee|
+        SmsTool.send_sms(number: employee.phone, message: notification_message)
+      end
     end
   end
 
   desc 'Sends Mail notification to managers each day to inform of pending
         overtime requests'
   task manager_email: :environment do
-    # 1. Iterate over the list of pending overtime requests
-    # 2. Check to see if there are any requests
-    # 3. Iterate over the list of admin users/managers
-    # 4. Send the email to each manager
     submitted_posts = Post.submitted
     admin_users = AdminUser.all
 
